33ca0edc35321c6dd5f3b788da2fcd59f9377ffa
catmellowz <giift19@gmail.com>
2023-02-06 15:49:16 +0700

    getme

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import jwtDecode from 'jwt-decode';
 import * as authApi from '../apis/auth-api';
 import {
@@ -13,6 +13,20 @@ export default function AuthContextProvider({ children }) {
   const [authenticatedUser, setAuthenticatedUser] = useState(
     getAccessToken() ? true : null
   );
+
+  useEffect(() => {
+    const fetchAuthUser = async () => {
+      try {
+        const res = await authApi.getMe();
+        setAuthenticatedUser(res.data.user);
+      } catch (err) {
+        removeAccessToken();
+      }
+    };
+    if (getAccessToken()) {
+      fetchAuthUser();
+    }
+  }, []);
 
   const login = async (emailOrUsername, password) => {
     const res = await authApi.login({ emailOrUsername, password });
