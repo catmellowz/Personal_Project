54c65aad9adec14d1d548b2a6116ae9b5b590909
catmellowz <giift19@gmail.com>
2023-02-08 16:03:27 +0700

    add status component

---
+++ src/components/Status.js
@@ -0,0 +1,9 @@
+export default function StatusOrder() {
+  return (
+    <div>
+      <div className='bg-yellow-500 rounded p-2 w-[100px]'>
+        on process
+      </div>
+    </div>
+  );
+}
--- src/pages/OrderHistory.js
+++ src/pages/OrderHistory.js
@@ -1,17 +1,30 @@
 import CartHistory from '../components/Cart/CartHistory';
 import Nav from '../components/Home/Navbar';
 import Footer from '../components/Home/Footer';
+import StatusOrder from '../components/Status';
 
 export default function OrderHistory() {
   return (
     <div>
-      <Nav />
-      <div className='m-10'>
-        <p className='flex justify-center text-3xl font-bold text-slate-900 '>
-          My Order
-        </p>
+      <div>
+        <Nav />
+        <div className='flex justify-center p-10'>
+          <div className='m-10'>
+            <p className='flex justify-center text-3xl font-bold text-slate-900 '>
+              My Order
+            </p>
+          </div>
+          <div className='flex'>
+            <CartHistory />
+            <div className=' flex items-center'>
+              <div className='flex justify-center bg-yellow-500 rounded p-2 w-[120px] h-[40px]'>
+                on process
+              </div>
+            </div>
+          </div>
+          {/* <StatusOrder /> */}
+        </div>
       </div>
-      <CartHistory />
       <Footer />
     </div>
   );
